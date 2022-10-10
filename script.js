8d39aba54ef116176f17aeef5f32a56bbaf9787f
Jonathan-Zim <2000yoni11@gmail.com>
2022-10-10 19:26:12 +0300

    Complete basic calculator design

---
+++ script.js
@@ -0,0 +1,16 @@
+let display = document.querySelector("display");
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function remainder(a, b) {
+    return a % b;
+}
