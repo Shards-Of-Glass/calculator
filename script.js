3bf0b98a95b06ac2cd796fadb737f61350fd9482
Jonathan Tsimbleris <a@gmail.com>
2024-07-21 10:08:11 +0300

    Mostly functional, except when a point is deleted

--- script.js
+++ script.js
@@ -1,95 +1,73 @@
-let display = document.querySelector("#display");
-let a = 0;
-let b = '';
-let displayVal = '';
-let func;
-const operator = document.querySelector('#operator');
-const btnVal = [];
-let ac = document.querySelector('#clear');
-ac.addEventListener('click', () => {
-    a = 0;
-    b = '';
-    displayVal = '';
-    display.textContent = '';
-})
+let displayVal='';
+let firstVal='';
+let secondVal='';
+let operand='';
+let point=false;
+let display=document.getElementById('display');
 
-for (let i = 0; i < 15; i++) {
-    btnVal[i] = document.getElementById(i);
+function append(value) {
+    if(point&&value=='.')
+        return;
+    if(value=='.')
+        point=true;
+    display.value += value;
 }
-{
-    for (let i = 0; i < 10; i++) {
-        if (b == '') {
-            btnVal[i].addEventListener('click', () => {
-                a = parseInt(a + '' + i);
-                if (displayVal == '') {
-                    a = i;
-                    displayVal = a;
-                }
-                display.textContent = a;
 
+function setOperand(val){
+    operand=val;
+    point=false;
+    firstVal=display.value;
+    display.value = '';
+}
 
-            })
+function calc(){
+    secondVal=display.value;
+    let result;
+    if(firstVal!=''&&secondVal!=''){
+        switch(operand){
+            case '+':
+                result=parseFloat(firstVal)+parseFloat(secondVal);
+                break;
+            case '-':
+                result=parseFloat(firstVal)-parseFloat(secondVal);
+                break;
+            case '*':
+                result=parseFloat(firstVal)*parseFloat(secondVal);
+                break;
+            case '/':
+                if(parseFloat(secondVal)!=0)
+                    result=parseFloat(firstVal)/parseFloat(secondVal);
+                else
+                    result='Error';
+                break;
+            case '^':
+                result=Math.pow(parseFloat(firstVal),parseFloat(secondVal));
+                break;
+            case '%':
+                result=parseFloat(firstVal)%parseFloat(secondVal);
+            default:
+                return;
         }
-
-    };
-}
-for (let i = 10; i < 15; i++) {
-    btnVal[i].addEventListener('click', () => {
-        func = i;
-        console.log(func)
-        if (b == '') b = 0;
-        if (b == '') {
-            for (let j = 0; j < 10; j++) {
-                {
-               
-                    btnVal[j].addEventListener('click', () => {
-                        b = parseInt(b + '' + j);
-                        display.textContent = b;
-                    })
-                }
-            };
-        }
-    })
+    
+    }
+    else
+        return;
+    display.value=result;
+    firstVal='';
+    secondVal='';
+    operand='';
 }
 
-operator.addEventListener('click', () => {
-    if (func == 10) { 
-        
-       console.log(add());
-    }
-    else if (func == 11) { 
-        console.log(b);
-       console.log(subtract());
-    }
-    else if (func == 12) { 
-        console.log(a);
-       console.log(multiply());
-    }
-   else if (func == 13) { 
-        console.log(a);
-       console.log(divide());
-    }
-    else if (func == 14) { 
-        console.log(a);
-       console.log(remainder());
-    }
-})
+function clearAll(){
+    display.value = '';
+    firstVal='';
+    secondVal='';
+    operand='';
+    point=false;
+}
 
-function add() {
-
-    return parseInt(a + b);
-}
-function subtract() {
-    return parseInt(a - b);
-}
-function multiply() {
-    return parseInt(a*b);
-}
-function divide() {
-    if (b == 0) return "Error";
-    else return parseInt (a / b);
-}
-function remainder() {
-    if (a < b) return 0;
-    return parseInt(a % b);
+function del(){
+    if(displayVal.charAt(displayVal.length-1)=='.')
+        point=false;
+    display.value = display.value.slice(0, -1);
 }
